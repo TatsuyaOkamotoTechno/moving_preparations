e6c84207a9266a5b7c03b116e83c13b64c89766f
Tatsuya Okamoto <Okamoto.Tatsuya@technopro.com>
2025-12-22 16:09:45 +0900

    springプロジェクトの作成

---
+++ src/main/java/com/example/moving_preparations/MovingPreparationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.moving_preparations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingPreparationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingPreparationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moving_preparations/MovingPreparationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moving_preparations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovingPreparationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
